a283daa2e5ebcbd66781d862159a956837a2638f
Olivia-Boyce-Richardson <124373077+Olivia-Boyce-Richardson@users.noreply.github.com>
2023-04-25 17:28:43 +0000

    js fixes

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
 // This file contains the JS functions for index.html
 
 'use strict'
-
+const randomNumber = Math.floor(Math.random() * 6) + 1  // returns a random integer from 1 to 6 into variable "randomNumber" 
 /**
  * This function uses a selection component from https://github.com/CreativeIT/getmdl-select
  */
