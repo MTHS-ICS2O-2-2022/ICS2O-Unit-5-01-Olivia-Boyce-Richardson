b093fd04c63645d06ffde4e1846df2f8e43fc6a4
Olivia-Boyce-Richardson <124373077+Olivia-Boyce-Richardson@users.noreply.github.com>
2023-05-12 16:44:26 +0000

    fixes

--- js/script.js
+++ js/script.js
@@ -1,26 +1,29 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Olivia B-R All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Olivia B-R
+// Created on: june 2023
 // This file contains the JS functions for index.html
 
-const randomNumber = Math.floor(Math.random() * 6) + 1;
+const randomNumber = Math.floor(Math.random() * 6) + 1
 
 /**
  * This function updates the slider value.
  */
-function updateSliderValue(valueFromSlider) { 
-  document.getElementById("slider-value").innerHTML = valueFromSlider
-}
+function myButtonClicked() {
+  // input
+  const numberGuessed = parseInt(
+    document.getElementById("guessed-number").value
+  )
 
-// process
-if (valueFromSlider == randomNumber) {
-  document.getElementById("answer").innerHTML =
-  "The answer was, " + randomNumber + "!" + "You got it! Good job."
-}
+  // process
+  if (numberGuessed == randomNumber) {
+    document.getElementById("answer").innerHTML =
+      "The answer was, " + randomNumber + "!" + "You got it! Good job."
+  }
 
-// block of code to be executed if condition1 is true
-if (valueFromSlider != randomNumber) {
-document.getElementById("answer").innerHTML =
-"The answer was, " + randomNumber + "!" + "Nice guess, but try again."
+  // block of code to be executed if condition1 is true
+  if (numberGuessed != randomNumber) {
+    document.getElementById("answer").innerHTML =
+      "The answer was, " + randomNumber + "!" + "Nice guess, but try again."
+  }
 }
